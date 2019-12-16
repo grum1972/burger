34d1b8205477fca9d54801cb3e1defb7692db2cb
Kotov Oleg <grum1972@gmail.com>
2019-12-17 02:26:05 +0300

    Сделал вертикальный аккордеон

--- main.js
+++ main.js
@@ -31,36 +31,8 @@ function menuClose() {
 }
 hamburgerMenu();
 
-// function controlVertAcco() {
-// 	const element = document.querySelector(".team__list");
-// 	const items	= document.querySelectorAll(".team__item");
-// 	element.addEventListener('click',e => {
-// 	e.preventDefault();
-// 	target = e.target;
-// 	let pt = target.parentElement;
-// 	console.log(pt);
-// 	let currentDesc = pt.querySelector(".team__desc");
-// 	if ((pt.classList.contains("team__item")&&(target.classList.contains("team__member")))) {
-// 		if (!pt.classList.contains("team__item--active")) {
-// 				items.forEach(function(item) {
-				
-// 					currentDesc.style.height = "0px";
-// 					item.classList.remove("team__item--active");
-// 			})
-// 			pt.classList.add("team__item--active");
-// 			currentDesc.style.height = currentDesc.scrollHeight +"px";
-// 		} else {
-// 			currentDesc.style.height = "0px";
-// 			pt.classList.remove("team__item--active");
-			
-// 		}
-// 	}
-// })
-	
-// }
-// controlVertAcco();
 
- function controlVertAcco1() {
+ function controlHorizAcco() {
 	const element = document.querySelector(".team__list");
 	element.addEventListener('click',e => {
 	e.preventDefault();
@@ -82,28 +54,60 @@ hamburgerMenu();
 	}
 	})
 };
-controlVertAcco1();
+controlHorizAcco();
+function calculateWidth() {
+	let windowWidth = window.innerWidth;
+	let links = document.querySelectorAll(".menu-burgers__link");
+	let reqWidth = windowWidth - links[0].offsetWidth * links.length;
+	return reqWidth > 550 ? 550 : reqWidth;
+};
 
-function controlHorAccor() {
+function controlVertAcco() {
 	const element = document.querySelector(".menu-burgers__list");
-	element.addEventListener('click',e => {
+	let body = document.querySelector("body");
+ 	element.addEventListener('click',e => {
 	e.preventDefault();
 	target = e.target;
-	let flagLink = target.classList.contains("team__member");
-	if (flagLink) {
-		curActive = element.querySelector(".team__item--active");
-		flagActive = target.parentElement.classList.contains("team__item--active");
+	curActive = element.querySelector(".menu-burgers__item--active");
+	if (target.classList.contains("menu-burgers__link") || target.classList.contains("menu-burgers__link-text")) {
 		if (curActive) {
-			  const activeDesc = curActive.querySelector(".team__desc");
-				activeDesc.style.height = "0px";
-				curActive.classList.remove("team__item--active");	
+				let activeText = curActive.querySelector(".menu-burgers__text");
+				activeText.style.width = "0px";
+				curActive.classList.remove("menu-burgers__item--active");	
+			
 		}
-		if (!curActive || curActive.querySelector(".team__member") !== target) {
-			target.parentElement.classList.add("team__item--active");
-			let currentDesc = target.parentElement.querySelector(".team__desc");
-			currentDesc.style.height = currentDesc.scrollHeight+"px";
-		}
+		if (!curActive || curActive !== target.closest(".menu-burgers__item")) {
+		
+			let activeText = target.closest(".menu-burgers__item").querySelector(".menu-burgers__text");
+				if (body.offsetWidth > 480) {
+					 activeText.style.width = calculateWidth() + "px";
+						
+				} else {
+					activeText.style.width = "100%";
+				}
+				target.closest(".menu-burgers__item").classList.add("menu-burgers__item--active");	 		
+		 }
+	} else {
+				let activeText = curActive.querySelector(".menu-burgers__text");
+				activeText.style.width = "0px";
+				curActive.classList.remove("menu-burgers__item--active");	
+	
 	}
+
 	})
 };
-controlHorAccor();
+controlVertAcco();
+
+function closeVertAcco() {
+ 	let body = document.querySelector("body");
+ 	body.addEventListener('click',e => {
+		 e.preventDefault();
+		 let ActiveAcco = document.querySelector(".menu-burgers__item--active");
+ 		 if (ActiveAcco && !e.target.closest(".menu-burgers__list")){
+			 let activeText = ActiveAcco.querySelector(".menu-burgers__text");
+			activeText.style.width = "0px";
+			ActiveAcco.classList.remove("menu-burgers__item--active");	
+		}	
+ 	})
+ };
+closeVertAcco() ;
